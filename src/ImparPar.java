e394515d892f2dddab38923fd06173bb404cf511
antoniobarreto <menabarreto14523@gmail.com>
2024-06-02 15:06:53 -0300

    alterei o nome da classe

--- src/ImparPar.java
+++ src/ImparPar.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class imparpar {
+public class ImparPar {
     public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
         System.out.println();
